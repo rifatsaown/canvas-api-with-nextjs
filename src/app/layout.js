7a474ff3731b58f3b29d21a7e1088e249f630fc6
Md. Rifat Hossen Saown <rifatsaown0@gmail.com>
2023-11-04 22:16:06 +0600

    Title Changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Next Test app',
+  title: 'Next Canvas app',
   description: 'A test app for Next.js',
 }
 
